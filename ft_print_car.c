e1e7dac059208ce5e5e2b1f72692c1cee9861e08
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-07-30 09:22:43 +0400

    Update ft_print_car.c

--- ft_print_car.c
+++ ft_print_car.c
@@ -1,2 +1,21 @@
 #include "ft_printf.h"
 
+int ft_printf(const char *format, ...)
+{
+while (*)
+{
+  if (*format == '%')
+    format++;
+
+while (*format)
+{
+  if (*format == '%')
+    format++;
+      if (*format == '%"){
+          Putchar('%')
+            counter++;
+}
+
+  
+  
+}
