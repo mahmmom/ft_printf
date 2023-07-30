0241d8b28186b5d8abd59b6b0bc3f7bea95aca0c
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-07-30 12:55:25 +0400

    Update ft_print.c

--- ft_print.c
+++ ft_print.c
@@ -1,45 +1,71 @@
 #include "ft_printf.h"
 
-int ft_printf(const char *format, ...)
+int	ft_print_formats (va_list args, const char format)
 {
-size_t  counter;
+int  counter;
 
 counter = 0;
-while (format[counter])
+if (format[i] == '%")
 {
-  if (format[counter] == '%')
-    format++;
-
-while (format[counter])
+  ft_Putchar('%')
+    counter++;
+}
+else if (format[counter] == 'c') 
 {
-  if (format[counter] == '%')
-    format++;
-      if (format[i] == '%")
-          ft_Putchar('%')
-            counter++;
+  int ch = va_arg(args, int)
+  ft_putchar(ch)
+  counter++;
+}
+else if (format[counter] == 's')
+{
+  int ch = va_arg(args, char*)
+  ft_strdup(*str)
+  counter++;
+}
+else if (format[counter] == 'd' || format[counter] == 'i')
+{
+  int ch = va_arg(args, int)
+  
+  counter++;
+}
+else if (format[counter] == 'x' || format[counter] == 'X')
+{
+  int ch = va_arg(args, int)
+  
+  counter++;
+}
+else if (format[counter] == 'p')
+{
+  int ch = va_arg(args, int)
+  
+  counter++;
+}
+  return (counter);
+}
 
-      else if (format[counter] == 'c') 
-      {
-          int ch = va_arg(args, int)
-          ft_putchar(ch)
-          counter++;
-      }
-      else if (format[counter] == 's')
-      {
-        int ch = va_arg(args, char*)
-        ft_strdup(*str)
-        counter++;
-      }
-      else
-      {
-        ft_strdup(*format);
-        counter++;
-      }
-  foramt++;
+
+int ft_printf(const char *format, ...)
+{
+  int  len;
+
+  len = 0;
+  while (format[len])
+  {
+    if (format[len] == '%')
+  {
+    len = len + ft_print_formats(args, str[len + 1]);
+    len++;
+  }
+  else
+  {
+    ft_strdup(*format);
+    counter++;
+  }
+  len++;
 }
 va_end(args);
 }
-return counter;      
+return (len);      
 }
 
 
