d28bbbab6bdf44ec55b73e1f20a1b7b9ffafa40e
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-07-30 10:41:21 +0400

    Update ft_print.c

--- ft_print.c
+++ ft_print.c
@@ -2,25 +2,64 @@
 
 int ft_printf(const char *format, ...)
 {
-while (*format)
+size_t  counter;
+
+counter = 0;
+while (format[counter])
 {
-  if (*format == '%')
+  if (format[counter] == '%')
     format++;
 
-while (*format)
+while (format[counter])
 {
-  if (*format == '%')
+  if (format[counter] == '%')
     format++;
-      if (*format == '%"){
+      if (format[i] == '%")
           Putchar('%')
             counter++;
-}
-   else if (*format == 'c') //if after % is c
+
+      else if (format[counter] == 'c') 
       {
-          int ch = va_arg(args, int) //Open variable to store the args from the list
-          putchar(ch) //print it
-          counter++; //increase our counter
+          int ch = va_arg(args, int)
+          putchar(ch)
+          counter++;
       }
-  
-  
+      else if (format[counter] == 's')
+      {
+        int ch = va_arg(args, char*)
+        putchar(*str)
+        counter++;
+      }
+      else if (format[counter] == 'd')
+      {
+        int ch = va_arg(args, int)
+        ft_atoi(*str)
+        counter++;
+      }
+      else if (format[counter] == 'i')
+      {
+        int ch = va_arg(args, int)
+        ft_atoi(*str)
+        counter++;
+      }
+      
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
