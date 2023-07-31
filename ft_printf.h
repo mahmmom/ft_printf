bcd2d8398d023aad31c6a937cb43fc283bec7494
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-07-31 09:19:02 +0400

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,13 @@
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-# include <stdarg.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
 
-
-
-
-
-
+int ft_printf(const char *format, ...);
 
 #endif
