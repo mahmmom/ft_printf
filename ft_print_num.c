177b24aa87cfbba62ed22264627c89410a99f4dc
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-08-01 09:58:06 +0400

    Create ft_print_num.c

---
+++ ft_print_num.c
@@ -0,0 +1,25 @@
+#include "ft_printf.h"
+
+int ft_print_num(int n)
+{
+    int count = 0;
+
+    if (n == -2147483648)
+    {
+        count += write(1, "-2147483648", 11);
+        return count;
+    }
+    else if (n < 0)
+    {
+        count += ft_putchar('-');
+        n = -n;
+    }
+
+    if (n > 9)
+    {
+        count += ft_print_num(n / 10);
+    }
+
+    count += ft_putchar(n % 10 + '0');
+    return count;
+}
