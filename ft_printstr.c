dfd656254ce300ad8d4f93af83b4d4fec3479b02
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-08-01 08:41:33 +0400

    Create ft_printstr.c

---
+++ ft_printstr.c
@@ -0,0 +1,15 @@
+int	ft_print_s(const char *str)
+{
+	int	len;
+
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	len = 0;
+	while (str[len])
+		len++;
+	write(1, str, len);
+	return (len);
+}
